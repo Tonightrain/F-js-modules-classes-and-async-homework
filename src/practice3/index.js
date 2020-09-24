206172dc114ffde3796ebbaee7ffd0d4cbcffcb1
xijing.gui <xijing.gui@thoughtworks.com>
2020-09-24 13:12:56 +0800

    add feedback

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,6 +1,8 @@
 export const getPoetry = () => fetch('https://v1.jinrishici.com/all.json')
     .then(response => response.json())
     .then(data => {
+
+            // TODO feedback: 可以用ES6解构 result
         let result = [];
         result.push(data.origin);
         result.push(data.author);
